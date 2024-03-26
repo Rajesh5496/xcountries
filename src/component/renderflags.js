edfd9c788e8997be8d364c574e385cf699572b4c
Rajeshvbridge <rajesh.r@vbridgehub.com>
2024-03-26 22:37:46 +0530

    changes

--- src/component/renderflags.js
+++ src/component/renderflags.js
@@ -6,36 +6,54 @@ import { useEffect, useState } from 'react'
 function Renderflags() {
     const baseURL = "https://restcountries.com/v3.1/all"
     const [flagdata, setflagdata] = useState([])
+    const [tempflagdata, setTempflagdata] = useState([])
     const [loading, setloading] = useState(true)
 
     useEffect(() => {
         axios.get(baseURL).then((response) => {
             console.log(response.data)
             setflagdata(response.data)
+            setTempflagdata(response.data)
             setloading(false)
         }).catch((error) => {
-            console.error('Error fetching data:', error);
+            console.log('Error fetching data:', error);
             setloading(true)
             // You can set an error state or handle the error in any other way here
         });
     }, [])
+
+    const handlechange = ((event)=>{
+        if(event.target.value == ''){
+            setflagdata(tempflagdata)
+        }else{
+            const filteredArray = flagdata.filter(obj => obj.name.common.includes(event.target.value));
+            console.log(filteredArray)
+            setflagdata(filteredArray)
+        }
+        
+    })
     return (
         <div>
             {loading ? (
                 <p>Loading...</p>
-            ): (
-            <div className={styles.container}>
-                {flagdata.map((ele) => (
-
-                    <div className={styles.box}>
-                        <img src={ele.flags.png} alt={ele.flags.alt} className={styles.imgclass} />
-                        <div>
-                            <p>{ele.name.common}</p>
-                        </div>
+            ) : (
+                <div>
+                    <div className={styles.inputdiv}>
+                        <input className={styles.inputcss} type='text' placeholder='Search for Countries' onChange={handlechange}/>
                     </div>
+                    <div className={styles.container}>
+                        {flagdata.map((ele) => (
 
-                ))}
-            </div>
+                            <div className={styles.countryCard}>
+                                <img key={ele} src={ele.flags.png} alt={ele.flags.alt} className={styles.imgclass} />
+                                <div>
+                                    <p>{ele.name.common}</p>
+                                </div>
+                            </div>
+
+                        ))}
+                    </div>
+                </div>
             )}
         </div>
     )
